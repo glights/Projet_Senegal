2935385571b1087c4bb1f77c8f41bd874abf9ee7
glights <tigui.03@gmail.com>
2015-03-18 16:19:57 -0400

    creation de la classe pour courant tension

---
+++ Classe_Capteurs/class_capteurs.cpp
@@ -0,0 +1,5 @@
+#include "class_capteurs.h"
+
+class_capteurs::class_capteurs()
+{
+}
---
+++ Classe_Capteurs/class_capteurs.h
@@ -0,0 +1,10 @@
+#ifndef CLASS_CAPTEURS_H
+#define CLASS_CAPTEURS_H
+
+class class_capteurs
+{
+public:
+    class_capteurs();
+};
+
+#endif // CLASS_CAPTEURS_H
