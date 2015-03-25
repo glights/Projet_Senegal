a1fc39edc583d2ebbda23cd9988aa1a6754b41a6
glights <tigui.03@gmail.com>
2015-03-25 10:58:31 -0400

    modif main

--- code_Main/code_Main.ino
+++ code_Main/code_Main.ino
@@ -1,2 +1,26 @@
+#include <DallasTemperature.h>
 
+#include <OneWire.h>
 
+#include <class_capteurs.h>
+
+/*
+*  Auteur : Guillaume Baril  & ilalio
+* 
+*
+**/
+
+class_capteurs capteurTC;
+
+void setup(void)
+{
+  Serial.begin(9600);
+  capteurTC.begin();
+}
+
+void loop(void)
+{
+  Serial.print("valeur:");
+  Serial.println(capteurTC.getCurrent());
+  delay(1000);
+}
